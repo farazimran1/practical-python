2b48952b445156e872d3c993d44d7c3e5bc0c0c9
Faraz Imran <farazimran12@gmail.com>
2024-09-25 21:57:59 -0400

    all code from modules 0 to 1.3

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+
+initial_height = 100
+current_height = initial_height
+bounce_factor = 3 / 5
+num_bounces = 0
+
+while num_bounces < 10:
+    num_bounces += 1
+    current_height *= bounce_factor
+    print(f"{num_bounces} {round(current_height, 4)}")
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+fixed_payment = 2684.11
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+total_paid = 0.0
+months = 0
+
+while principal > 0:
+    months += 1
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        payment = fixed_payment + extra_payment
+    else:
+        payment = fixed_payment
+
+    if payment > principal:
+        payment = principal * (1 + rate / 12)
+
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+    print(f"{months} {round(total_paid, 2)} {round(principal, 2)}")
+
+print("Total paid", round(total_paid, 2))
+print("Months", months)
