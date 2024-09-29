6def9aef7cf3cbb1a6c7e7090e7afb5b92c34618
Faraz Imran <farazimran12@gmail.com>
2024-09-28 20:22:05 -0400

    improved pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,12 +2,26 @@
 #
 # Exercise 1.27
 
-total_cost = 0
+import csv
+import sys
 
-with open("Data/portfolio.csv", "+rt") as file:
-    next(file)
-    for line in file:
-        row = line.split(",")
-        total_cost += int(row[1]) * float(row[2])
 
-print(f"Total cost {total_cost:.2f}")
+def portfolio_cost(filename):
+    with open(filename, "+rt") as file:
+        rows = csv.reader(file)
+        next(rows)
+        total_cost = 0
+        for row in rows:
+            try:
+                total_cost += int(row[1]) * float(row[2])
+            except (ValueError, IndexError):
+                print("couldn't parse line")
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    cost = portfolio_cost(sys.argv[1])
+    print(f"Total cost {cost:.2f}")
+else:
+    print("usage: python pcost.py <portfolio.csv>")
+    sys.exit(1)
